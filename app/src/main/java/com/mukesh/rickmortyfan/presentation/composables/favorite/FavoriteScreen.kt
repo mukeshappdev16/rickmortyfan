7bc07cd5e1810a595995994e87f4025238eb4d95
mukesh <mukesh.appdev16@gmail.com>
2026-04-17 08:21:29 +0530

    Branch rule test

--- app/src/main/java/com/mukesh/rickmortyfan/presentation/composables/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/mukesh/rickmortyfan/presentation/composables/favorite/FavoriteScreen.kt
@@ -10,6 +10,6 @@ import androidx.compose.ui.Modifier
 @Composable
 fun FavoriteScreen() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Hello there, this is empty")
+        Text("Hello there, this is empty.")
     }
 }
