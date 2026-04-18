3e27ffc6535e8c1dcebb3ee813761bdf2e4d4b08
mukesh <mukesh.appdev16@gmail.com>
2026-04-18 11:09:02 +0530

    Infinite loading implemented for episode

--- app/src/main/java/com/mukesh/rickmortyfan/domain/repository/EpisodesRepository.kt
+++ app/src/main/java/com/mukesh/rickmortyfan/domain/repository/EpisodesRepository.kt
@@ -4,7 +4,7 @@ import com.mukesh.rickmortyfan.domain.modal.episode.Episode
 import com.mukesh.rickmortyfan.domain.modal.episode.Episodes
 
 interface EpisodesRepository {
-    suspend fun getAllEpisodes(): Episodes
+    suspend fun getAllEpisodes(page: Int): Episodes
     suspend fun getEpisodeDetail(episodeId: String): Episode
     suspend fun getMultipleEpisodes(ids: List<String>): List<Episode>
 }
--- app/src/main/java/com/mukesh/rickmortyfan/presentation/composables/episode/episodelist/EpisodeListState.kt
+++ app/src/main/java/com/mukesh/rickmortyfan/presentation/composables/episode/episodelist/EpisodeListState.kt
@@ -4,7 +4,10 @@ import com.mukesh.rickmortyfan.domain.modal.episode.Episode
 
 data class EpisodeListState(
     val isLoading: Boolean = false,
+    val isPaginating: Boolean = false,
     val errorMessage: String = "",
     val noInternet: Boolean = false,
-    val list: List<Episode> = emptyList()
+    val list: List<Episode> = emptyList(),
+    val page: Int = 1,
+    val endReached: Boolean = false
 )
